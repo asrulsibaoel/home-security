7b1c5e7bfceada7a29bdb87e20c6fedb7a7e878a
asrulsibaoel <asrulsibaoel@users.noreply.github.com>
2022-09-14 15:20:04 +0000

    ✅ Ready to clone and code.

--- home_security/app.py
+++ home_security/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+home_security API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="home_security",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://home_security.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "asrulsibaoel",
+        "url": "http://home_security.com/contact/",
+        "email": "asrulsibaoel@home_security.com",
     },
     license_info={
         "name": "The Unlicense",
